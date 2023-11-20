c63df89f72ec0023d51baf3cf85806fa43451de1
Anssi Äijö <anssi.aijo@edu.karelia.fi>
2023-11-20 09:33:34 +0200

    blogilistan laajennus

---
+++ bloglist/backend/index.js
@@ -0,0 +1,7 @@
+const app = require('./app')
+const { PORT , MONGODB_URI } = require('./utils/config')
+const logger = require('./utils/logger')
+
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
+})
